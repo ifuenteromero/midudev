72376e5ed964a8058f11e2ff2a4babf67133a5e9
Irene Fuente Romero <ifuenteromero@gmail.com>
2023-11-14 15:44:34 +0100

    Initial commit

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,21 @@
+const App = () => {
+	return (
+		<article>
+			<header>
+				<img
+					src="https://unavatar.io/midudev"
+					alt="El avatar de midudev"
+				/>
+				<div>
+					<strong>Miguel Ángel Durán</strong>
+					<span>@midudev</span>
+				</div>
+			</header>
+			<aside>
+				<button>Seguir</button>
+			</aside>
+		</article>
+	);
+};
+
+export default App;
